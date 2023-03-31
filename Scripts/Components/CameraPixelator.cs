5fbe8a42ce25c2e8ce06e39328b8614d8583e26d
Callum Else <callum.else@gmail.com>
2023-03-31 18:20:11 +0100

    CameraPixelator Target Camera Fix
    Updated CameraPixelator to actually target the camera attached its gameobject.

--- Scripts/Components/CameraPixelator.cs
+++ Scripts/Components/CameraPixelator.cs
@@ -9,6 +9,18 @@ namespace SimplePixels
     {
         [SerializeField, Range(64.0f, 512.0f)] private float _blockCount = 128;
 
+        private Camera _camera;
+        private Camera Camera
+        {
+            get
+            {
+                if (_camera == null)
+                    _camera = GetComponent<Camera>();
+
+                return _camera;
+            }
+        }
+
         private Material _material;
         private Material Material
         {
@@ -19,13 +31,14 @@ namespace SimplePixels
                     _material = new Material(Shader.Find("Hidden/CameraPixelator"));
                     _material.hideFlags = HideFlags.HideAndDontSave;
                 }
+
                 return _material;
             }
         }
 
         private void OnRenderImage(RenderTexture source, RenderTexture destination)
         {
-            float camAspect = Camera.main.aspect;
+            float camAspect = Camera.aspect;
             Vector2 count = new Vector2(_blockCount, _blockCount/camAspect);
             Vector2 size = new Vector2(1.0f/count.x, 1.0f/count.y);
             Material.SetVector("BlockCount", count);
